b39f03d978c8cf9080aca543f1add90c8e3f9dfe
kircatle <kircatle@gmail.com>
2020-10-02 01:42:19 +0300

    i add 1 task 4 week

---
+++ Week4/coursera_assignment_tmp/routing/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse
+from django.views.decorators.http import require_GET, require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+
+def simple_route(request):
+    if request.method == "GET":
+        return HttpResponse(status="200") 
+    else:
+        return HttpResponse(status="405") 
+
+
+def slug_route(request, slug):
+    return HttpResponse(slug, status="200")
+
+
+def sum_route(request, num1, num2):
+    return HttpResponse(int(num1)+int(num2), status="200") 
+
+
+@require_GET
+def sum_get_method(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    try:
+        return HttpResponse(int(a)+int(b), status="200")
+    except ValueError:
+        return HttpResponse(status="400") 
+
+
+@csrf_exempt
+@require_POST
+def sum_post_method(request):
+    a = request.POST['a']
+    b = request.POST['b']
+    try:
+        return HttpResponse(int(a)+int(b), status="200")
+    except ValueError:
+        return HttpResponse(status="400") 
+
+
